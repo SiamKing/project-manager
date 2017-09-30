19be29f414788fd25f6b81da5680ddcadf56df81
SiamKing <thesiameseelectric@gmail.com>
2017-09-29 19:50:08 -0700

    added delete function for projects

--- src/App.js
+++ src/App.js
@@ -39,12 +39,19 @@ class App extends Component {
     this.setState({projects: projects});
   }
 
+  handleDeleteProject = (id) => {
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id)
+    projects.splice(index, 1);
+    this.setState({projects: projects});
+  }
+
   render() {
     return (
       <div className="App">
         <AddProject addProject={this.handleAddProject}/>
         <br />
-        <Projects projects={this.state.projects} />
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject}/>
       </div>
     );
   }
